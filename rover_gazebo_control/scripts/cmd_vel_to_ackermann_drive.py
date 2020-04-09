6b17ae4127d5d79723a5943cb111ae799430aa53
Ravi Sharma <s2007.ravi@gmail.com>
2020-04-09 15:06:10 +0530

    Corrected publisher topic (included namespace). Fetch correct value of wheelbase.

--- rover_gazebo_control/scripts/cmd_vel_to_ackermann_drive.py
+++ rover_gazebo_control/scripts/cmd_vel_to_ackermann_drive.py
@@ -37,11 +37,19 @@ def cmd_callback(data):
 if __name__ == '__main__':
     try:
         rospy.init_node('cmd_vel_to_ackermann_drive')
-
+        _wheelbase = 1.0
+        namespace = rospy.get_namespace()
         twist_cmd_topic = rospy.get_param('~twist_cmd_topic', '/cmd_vel')
-        ackermann_cmd_topic = rospy.get_param('~ackermann_cmd_topic', '/ackermann_cmd')
-        wheelbase = rospy.get_param('~wheelbase', 1.0)
+        ackermann_cmd_topic = rospy.get_param('~ackermann_cmd_topic', namespace + 'ackermann_cmd')
         frame_id = rospy.get_param('~frame_id', 'odom')
+        try:
+            wheelbase = rospy.get_param(namespace + "wheelbase")
+            if wheelbase <= 0.0:
+                raise ValueError()
+        except Exception:
+            rospy.logwarn("The specified wheelbase value is invalid."
+                          "Instead, the default wheelbase value will be used.")
+            wheelbase = _wheelbase
 
         rospy.Subscriber(twist_cmd_topic, Twist, cmd_callback, queue_size=1)
         pub = rospy.Publisher(ackermann_cmd_topic, AckermannDriveStamped, queue_size=1)
